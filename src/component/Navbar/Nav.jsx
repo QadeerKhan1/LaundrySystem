2d97d8be880dd9c7d6f1b2c6373fb201bca8ac37
QadeerKhan1 <qadeer1911@gmail.com>
2023-02-15 11:02:41 +0500

    NavbarLogo
    the navbar logo has been changed

--- src/component/Navbar/Nav.jsx
+++ src/component/Navbar/Nav.jsx
@@ -11,7 +11,7 @@ export default function Navbar() {
   <div className="navbarDiv">
   <nav class="navbar navbar-expand-lg navbar-light  ">
   <div class="container-fluid  " id='navMain'>
-    <a class="navbar-brand" href="#"><img src={logo} alt="" style={{width:'120px',height:'100px'}} /></a>
+    <a class="navbar-brand" href="#"><img src={logo} alt="" style={{width:'120px',height:'100px' }} /></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">      <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse allitem" id="navbarNav">
